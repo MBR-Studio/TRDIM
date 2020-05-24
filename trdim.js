6857ede4c6f919fd4ff7b06f1b6ede5584b014df
RuiLiang <55020690+MBRjun@users.noreply.github.com>
2020-05-24 15:40:20 +0800

    Update trdim.js

--- trdim.js
+++ trdim.js
@@ -1,3 +1,22 @@
+player.onChat("./ban", function () {
+    blocks.place(MAGMA_BLOCK, pos(0, -1, 0))
+    blocks.place(WATER, pos(0, 2, 0))
+    blocks.place(LAVA, pos(0, 3, 0))
+    loops.pause(1000)
+    blocks.place(WHITE_CARPET, pos(0, 0, 0))
+    blocks.place(OBSIDIAN, pos(1, 3, 0))
+    blocks.place(OBSIDIAN, pos(-1, 3, 0))
+    blocks.place(OBSIDIAN, pos(0, 3, 1))
+    blocks.place(OBSIDIAN, pos(0, 3, -1))
+    blocks.place(OBSIDIAN, pos(1, 0, 0))
+    blocks.place(OBSIDIAN, pos(-1, 0, 0))
+    blocks.place(OBSIDIAN, pos(0, 0, 1))
+    blocks.place(OBSIDIAN, pos(0, 0, -1))
+    blocks.place(GLASS, pos(1, 1, 0))
+    blocks.place(GLASS, pos(-1, 1, 0))
+    blocks.place(GLASS, pos(0, 1, 1))
+    blocks.place(GLASS, pos(0, 1, -1))
+})
 player.onChat("./tr", function () {
     for (let index = 0; index < 10; index++) {
         mobs.spawn(PRIMED_TNT, pos(0, 32, 0))
@@ -31,4 +50,4 @@ player.onChat("./dim", function () {
     )
 })
 player.say("Minecraft Play - MBRjun.cn 载入成功")
-player.say("输入 ./tr ./dim 激活")
+player.say("输入 ./tr ./dim ./ban激活")
